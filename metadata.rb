bd8d670958ebef26ae1daca460c1902ca5dc7c8a
Nathan Mische <nmische@gmail.com>
2012-09-13 14:18:59 -0400

    Added metadata

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "NATHAN MISCHE"
+maintainer_email "nmische@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures MXUnit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+depends "coldfusion902"
+
+recipe "mxunit", "Installs MXUnit and adds a ColdFusion mapping."
