688a3cdef705f2a1234d55dac64ae5d0cf6248c5
John Piotrowski <johnrp@wharton.upenn.edu>
2012-10-22 08:47:40 -0300

    Update attributes/default.rb
    change the install path to /vagrant/frameworks install of in the wwwroot since there is a mapping created

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # MXUnit Attributes
-default['mxunit']['install_path'] = "/vagrant/wwwroot"
+default['mxunit']['install_path'] = "/vagrant/frameworks"
 default['mxunit']['download']['url'] = "https://github.com/downloads/mxunit/mxunit/mxunit-2.1.1.zip"
 
 
