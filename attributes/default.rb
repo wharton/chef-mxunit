aac8658a1b7abae4bb7cfd1cf29170e23f442036
Nathan Mische <nmische@gmail.com>
2013-09-27 11:56:19 -0400

    Updated default mxunit version. Updated apache template.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 default['mxunit']['install_path'] = "/vagrant/wwwroot"
 default['mxunit']['owner'] = nil
 default['mxunit']['group'] = "bin"
-default['mxunit']['download']['url'] = "https://github.com/downloads/mxunit/mxunit/mxunit-2.1.1.zip"
+default['mxunit']['download']['url'] = "https://github.com/downloads/mxunit/mxunit/mxunit-2.1.3.zip"
 default['mxunit']['create_apache_alias'] = false
 
 
